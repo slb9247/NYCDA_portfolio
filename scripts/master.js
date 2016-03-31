1fb877e0000d259f93688cd69ea29a07384b2c85
Samantha Bolotine <slb1992@yahoo.com>
2016-03-31 15:57:44 -0400

    deleted photo page js

--- scripts/master.js
+++ scripts/master.js
@@ -1,34 +1,3 @@
 $(document).ready(function() {
-    $('#photo1').hover(
-      function() {
-        $('div.hide1').toggleClass('show');
-      }
-    ),
-    $('#photo2').hover(
-      function() {
-        $('div.hide2').toggleClass('show');
-      }
-    ),
-    $('#photo3').hover(
-      function() {
-        $('div.hide3').toggleClass('show');
-      }
-    ),
-    $('#photo4').hover(
-      function() {
-        $('div.hide4').toggleClass('show');
-      }
-    ),
-    $('#photo5').hover(
-      function() {
-        $('div.hide5').toggleClass('show');
-      }
-    ),
-    $('#photo6').hover(
-      function() {
-        $('div.hide6').toggleClass('show');
-      }
-    );
-
-    $('#masthead').fadeIn(1500);
+    $('.mastheadBig').fadeIn(1500);
 });
